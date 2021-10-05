f09b8685fc477e396983088526417b078f3cbb35
yangyt96 <yeeyang.tan@live.com>
2021-10-05 13:03:02 +0200

    [chore] share common nocpe resources

--- src/main.c
+++ src/main.c
@@ -1,11 +1,31 @@
 #include <argp.h>
+#include <time.h>
 
-#include "nocpe_full.h"
-#include "nocpe_random.h"
-#include "nocpe_netrace.h"
+#include "nocpe/nocpe_full.h"
+#include "nocpe/nocpe_random.h"
+#include "nocpe/nocpe_netrace.h"
 
 int main(int argc, char *argv[])
 {
+
+    // general
+    NocPe_Resource.max_cyc = 1000;
+    NocPe_Resource.seed = time(0);
+    srand(NocPe_Resource.seed);
+
+    // full
+    NocPe_Resource.pkt_len = NOCPE_PKT_MAX_LEN;
+    NocPe_Resource.time_step = 50;
+
+    // random
+    NocPe_Resource.min_time_step = 10;
+    NocPe_Resource.max_time_step = 30;
+
+    // netrace
+    NocPe_Resource.trace_file = "multiregion.tra";
+    NocPe_Resource.start_region = 0;
+    NocPe_Resource.ignore_dependencies = 1;
+    NocPe_Resource.reader_throttling = 0;
 
     if (argc == 1)
         nocpe_full_run();
