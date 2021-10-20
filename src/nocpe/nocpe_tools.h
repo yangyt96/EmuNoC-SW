841557504e8e798443d1a001a58e8e7a9187dc3d
yangyt96 <yeeyang.tan@live.com>
2021-10-20 23:35:14 +0200

    [feat] bool include

--- src/nocpe/nocpe_tools.h
+++ src/nocpe/nocpe_tools.h
@@ -20,6 +20,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#include <stdbool.h>
+
 #include "nocpe.h"
 #include "cctrlib/list.h"
 #include "xaxidma/xaxidma_sg.h"
