3a5a9fab2b166810bdc2764f33f530c24842c976
yangyt96 <yeeyang.tan@live.com>
2022-06-23 21:01:13 +0200

    [doc] update license

--- src/xaxidma/xaxidma_bd.h
+++ src/xaxidma/xaxidma_bd.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021 Yee Yang Tan
+Copyright (c) 2021 Yee Yang Tan (https://github.com/yangyt96/plnx-uiolib)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
